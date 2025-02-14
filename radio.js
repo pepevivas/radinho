866d912974eb09ba1a1a6fdf2e9f06488ed2b545
tuliogrossi <163157544+tuliogrossi@users.noreply.github.com>
2025-02-13 22:43:34 -0300

    Add files via upload

--- radio.js
+++ radio.js
@@ -1,13 +1,13 @@
-
+let comprasSalvas = [];
+const pontosDisplay = document.getElementById("pontos-display");
 let botao1EL = document.querySelector('.botao1');
 let BogaograndaoEL = document.querySelector('.bogagrandao');
 let comprasEL = document.querySelector('.compras');
-
-let pontos = 0;
-const pontosDisplay = document.getElementById("pontos-display");
+let multiplicador = 1;
+let pontos;
 
 BogaograndaoEL.addEventListener("click", function () {
-    pontos++;
+    pontos += multiplicador;
     pontosDisplay.textContent = pontos;
 });
 
@@ -32,15 +32,15 @@ Object.keys(escolherCompras).forEach(menuID => { //francisco me ensinou muito ma
     botao.addEventListener("click", function () {
         Object.values(escolherCompras).forEach(aba => aba.style.display = "none");
         escolherCompras[menuID].style.display = "grid";
-        escolherCompras[menuID].style.gridTemplateColumns= "auto auto auto";
-        escolherCompras[menuID].style.gridTemplateRows= "auto auto";
+        escolherCompras[menuID].style.gridTemplateColumns = "auto auto auto";
+        escolherCompras[menuID].style.gridTemplateRows = "auto auto";
     });
 });
 
 
 
 //compras 
-let bodyEL=document.querySelector('body');
+let bodyEL = document.querySelector('body');
 let produtos = document.querySelectorAll(".produto");
 let backgrounds = [
     "url('imgs/jardimBIZARRO.png')",
@@ -49,43 +49,88 @@ let backgrounds = [
     "url('imgs/camposverdejantes.jpg')",
     "url('imgs/STEVE.jpg')"
 ];
-let cadeadoEl = document.querySelectorAll(".cadeado");
+let cadeadoEL = document.querySelectorAll(".cadeado");
 let mensagem = document.querySelectorAll(".multiplicador");
 
 produtos.forEach(produto => {
-    
+
     let nome = produto.dataset.nome;
     let item = produto.dataset.tipo;
     let preco = produto.dataset.preco;
+    let modificador = produto.dataset.multiplicado;
     let cadeado = produto.dataset.cadeado;
     produto.addEventListener("click", function () {
-        console.log(cadeadoEl[cadeado]);
+        console.log(cadeadoEL[cadeado]);
         if (produto.classList.contains("bloqueado")) {
-            if (pontos >= preco) 
-                {
+            if (pontos >= preco) {
                 pontos -= preco;
                 pontosDisplay.textContent = pontos;
                 produto.classList.remove("bloqueado");
-                cadeadoEl[cadeado].style.display = "none";
-            } 
+                cadeadoEL[cadeado].style.display = "none";
+                //salvar a compra do produto
+                for (let i = 0; i < produtos.length; i++) {
+                    if (produtos[i].classList.contains('bloqueado')) {
+                        comprasSalvas[i] = 0;
+                    }
+                    else {
+                        comprasSalvas[i] = 1;
+                    }
+
+                }
+                localStorage.setItem('compras', JSON.stringify(comprasSalvas));
+            }
             else {
                 alert("POBRE HAHAHAHAH");
             }
         }
-         else if (item == "fundo")
-        {
+        else if (item == "fundo") {
             let imagem = backgrounds[nome];
             bodyEL.style.backgroundImage = imagem;
         }
         else if (item === "bixos") {
             let bixosDoidosEL = document.querySelector(`.imagem.${nome}`);
             if (bixosDoidosEL.style.display === "none" || bixosDoidosEL.style.display === "") {
+                multiplicador *= modificador;
                 bixosDoidosEL.style.display = "block";
-            } 
+            }
             else {
+                multiplicador /= modificador;
                 bixosDoidosEL.style.display = "none";
             }
-        } 
-        
+
+        }
+
     });
 });
+//salvar pontos
+BogaograndaoEL.addEventListener('click', () => {
+    window.localStorage.setItem('pontos', pontos);
+});
+//carregar pontos 
+if (window.localStorage.getItem('pontos')) {
+    pontos = parseInt(window.localStorage.getItem('pontos'));
+}
+else {
+    pontos = 0;
+}
+pontosDisplay.textContent = pontos;
+if (window.localStorage.getItem('produtos')) {
+    comprasSalvas = window.localStorage.getItem('produtos');
+    comprasSalvas = JSON.parse(comprasSalvas);
+    console.log(comprasSalvas);
+}
+//carregar compra
+console.log(localStorage.getItem('compras'));
+if (localStorage.getItem('compras'))
+{
+    console.log('fghjfh');
+    comprasSalvas = localStorage.getItem('compras');
+    comprasSalvas = JSON.parse(comprasSalvas);
+    for (let i = 0; i < comprasSalvas.length; i++) {
+        if (comprasSalvas[i] == 1) {
+            let temp = produtos[i];
+            temp.classList.remove('bloqueado');
+            cadeadoEL[temp.dataset.cadeado].style.display = 'none';
+        }
+    }
+}
